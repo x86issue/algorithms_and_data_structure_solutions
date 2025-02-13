34db323b9124a3a06118ab77835ca40ce262fb05
user <user@TANKISTV2>
2025-02-13 23:13:01 +0700

    Добавьте файлы проекта.

---
+++ Lession1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lession1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int res = SumOfDigitOfNumbers(1, 100);
+            Console.WriteLine(res);
+
+        }
+
+        static int SumOfDigitOfNumbers(int left, int right)
+        {
+            int sum = 0;
+            for (int number = left; number <= right; number++)
+            {
+
+                int tmp = number;
+                while (tmp > 0)
+                {
+                    if (tmp % 2 == 0)
+                        sum += tmp % 10;
+                    tmp /= 10;
+
+                }
+            }
+            return sum;
+        }
+    }
+}
